8f56b48ebc2a47db2c3f061628a2f7811d8ae0c6
Cristhian Parra <cdparra@gmail.com>
2014-01-10 14:37:32 +0100

    added person name, added onlylocal option for generating a pdf without using external HTTP urls

--- scripts/python/stories-export.py
+++ scripts/python/stories-export.py
@@ -25,7 +25,10 @@ def download_picture(url,name):
     except IOError as e:
     	print( "Download ERROR" + str(e))
 
-def convert_to_pdf(file): 
+def convert_to_pdf(file):
+	# Beware that when there are two many picture urs in a story, two erros might happen
+	# 1. Latex's "Too many unprocessed floats" 
+	# 2. Random URL's not being able to download
 	print("Converting ", file.name, " to PDF...")
 	out_file = file.name+".pdf"
 
@@ -51,22 +54,22 @@ def print_avviso(file, person_name):
 	avviso += "completo e ti invitiamo a correggere dei problemi o errori che trovi "
 	avviso += "attraverso la pagina web di reminiscens (base.reminiscens.me/ui), "
 	avviso += "al massimo prima della mattina del **15/gennaio.** \n\n"
-	avviso += "Se non riesci a correggere tutto per il 15/gennaio, ti chidiamo "
+	avviso += "Se non riesci a correggere tutto per il 15/gennaio, ti chiediamo "
 	avviso += "gentilmente di indicare ai ricercatori quali sono i problemi/errori da "
-	avviso += "corregere tramite un'email a **cdparra@gmail.com**, oppure consegnando "
+	avviso += "correggere tramite un'email a **cdparra@gmail.com**, oppure consegnando "
 	avviso += "una copia di questa bozza con i commenti.\n\n"
 	avviso += "Inoltre, ti chiediamo di **scegliere 3 o 4 storie preferite che saresti "
-	avviso += "d'accordo ad includere in un libro finale generale che sará consegnato al CSA.** "
+	avviso += "d'accordo ad includere in un libro finale generale che sarà consegnato al CSA.** "
 	avviso += "Invia il numero e titolo di queste storie tramite email a **cdparra@gmail.com** "
 	avviso += "prima del **19/gennaio.** \n\n"
-	avviso += "Osservazioni: \n\n"
+	avviso += "Alcune Osservazioni: \n\n"
 	avviso += "* Se hai già controllato e corretto tutto prima, no c'e bisogno di farlo di nuovo\n"
 	avviso += "* Questa è solo una bozza per controllo, non è ancora impaginata come "
 	avviso += "sarà nella versione finale (che certamente sarà molto più bella)\n"
 	avviso += "* L'orientazione e dimensione delle foto saranno sistemate/ottimizzate "
 	avviso += "nel libro finale. Le foto in questa bozza sono soltanto una anteprima con "
 	avviso += "l'obiettivo di facilitare il controllo.\n\n"
-	avviso += "* Per alcuni errori di sistema, e possibile che in alcune storie la 'domanda collegata' "
+	avviso += "* A causa di errori di sistema, e possibile che in alcune storie la 'domanda collegata' "
 	avviso += "sia sbagliata. Non devi preoccuparti di questi errori, saranno sistemati automaticamente.\n\n"
 	avviso += "* Una copia stampata della bozza sarà disponibile al CSA.\n\n"
 	avviso += "Vi saluta cordialmente,\n Il team di Reminiscens\n\n\n"
@@ -83,6 +86,7 @@ parser.add_argument('outtesto', help='output file for stories', type=argparse.Fi
 parser.add_argument('outmeta', help='output file for stories', type=argparse.FileType('a', encoding='UTF-8'),default=sys.stdout)
 parser.add_argument('outfull', help='output file for stories with embedded pictures', type=argparse.FileType('a', encoding='UTF-8'),default=sys.stdout)
 parser.add_argument('person', help='name of the person', type=str)
+parser.add_argument('--onlylocal', help='skip story processing and jumpt to pdf generations from output files', action='store_true')
 parser.add_argument('--onlypdf', help='skip story processing and jumpt to pdf generations from output files', action='store_true')
 args = parser.parse_args()
 
@@ -94,6 +98,8 @@ storydata = args.outmeta
 storywithpics = args.outfull
 onlypdf = args.onlypdf
 person_name = args.person
+onlylocal = args.onlylocal
+
 
 if onlypdf:
 	convert_to_pdf(storyout)
@@ -219,23 +225,13 @@ with open(csv_file, 'rt', encoding='iso-8859-1') as csvfile:
 			picname_for_markdown = re.sub('[)]', '\)', picname)
 			storypath = storyid + "_" + title 
 			
+			# Donwload LARGE picture for BOOK
 			dirpath = os.path.abspath(os.path.join(picsdir,storypath))
 			dirpath.strip()
 			if not os.path.exists(dirpath):
 				os.makedirs(dirpath)
 			path = os.path.join(dirpath,picname)
 			path.strip()
-
-			# embed picture in the output markdown file with embedded pictures
-			#browserpath = "file://localhost"+path
-			#img = "** Foto # "+photoid+" **\n"
-			#img += "<img src=\""+browserpath+"\" width=\"200\"/>"
-			#storywithpics.write(img+"\n\n")
-			print( "### Printing picture "+fotohash+" to "+storywithpics.name)
-
-			pic_small_url = "http://base.reminiscens.me/lifeapi/file/"+fotohash+"/SMALL"
-			img = "![Foto "+photoid+" della storia "+storyid+"]("+pic_small_url+")"
-			storywithpics.write(img+"\n\n")
 
 			# do not download twice
 			if not os.path.exists(path):
@@ -244,6 +240,43 @@ with open(csv_file, 'rt', encoding='iso-8859-1') as csvfile:
 				download_picture(url,path)
 			else: 
 				print( "Already downloaded picture ("+photoid+") "+picname+" of story ("+storyid+") '"+title+"'")
+
+			
+			pic_small_url = "http://base.reminiscens.me/lifeapi/file/"+fotohash+"/SMALL"
+			url = pic_small_url
+
+			if onlylocal: 
+
+				# Donwload SMALL picture for control DRAFT
+				dirpath = "/Users/cristhian/Dropbox/research/projects/reminiscens/reminiscens-books/SMALLs"
+				dirpath.strip()
+				filename = fotohash+".jpg"
+				if not os.path.exists(dirpath):
+					os.makedirs(dirpath)
+				path = os.path.join(dirpath,filename)
+				path.strip()
+
+				
+				# do not download twice
+				if not os.path.exists(path):
+					print( "### Downloading SMALL picture ("+photoid+") "+picname+" with hash ("+fotohash+")")
+					print( "### Copying in "+path+"###")
+					download_picture(pic_small_url,path)
+				else: 
+					print( "Already downloaded picture ("+photoid+") "+picname+" of story ("+storyid+") '"+title+"'")
+
+			# embed picture in the output markdown file with embedded pictures
+			#browserpath = "file://localhost"+path
+			#img = "** Foto # "+photoid+" **\n"
+			#img += "<img src=\""+browserpath+"\" width=\"200\"/>"
+			#storywithpics.write(img+"\n\n")
+			print( "### Printing picture "+fotohash+" to "+storywithpics.name)
+
+			if onlylocal:
+				url = re.sub('[\s]', '\\ ', path)
+
+			img = "![Foto "+photoid+" della storia "+storyid+"]("+url+")"
+			storywithpics.write(img+"\n\n")
 
 		else: 
 			print( "Story '"+title+"' has no valid URL associated")
